e4d2d4fc45f3a42f0336310c3d41fa6b729d8b10
Sofia Bracho <sofia_bracho1@hotmail.com>
2024-08-28 15:03:40 -0400

    captcha

--- src/app/pages/register/components/register-form/register-form.component.ts
+++ src/app/pages/register/components/register-form/register-form.component.ts
@@ -12,11 +12,12 @@ import { Loader } from 'src/app/app.models';
 import { SharedModule } from 'src/app/components/shared.module';
 import { CustomFields } from '../../register.component';
 import { NotificationStore } from 'src/app/stores/notification.store';
+import { RecaptchaModule, RecaptchaFormsModule } from 'ng-recaptcha';
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [NgIf, NgFor, SharedModule],
+  imports: [NgIf, NgFor, SharedModule, RecaptchaModule, RecaptchaFormsModule],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.scss',
 })
@@ -29,6 +30,8 @@ export class RegisterFormComponent implements OnInit {
   @Input() loader: Loader = new Loader(false, false, false);
   @Input() formStatus: any = { isInvalid: false, message: '' };
   fileName: string = '';
+  siteKey: string = '6LdvwTAqAAAAACEDkpCUKon-wDYiuI3z0AtU216u';
+  captchaResolved: boolean = false;
   ngOnInit(): void {}
 
   handleSubmit() {
@@ -58,5 +61,9 @@ export class RegisterFormComponent implements OnInit {
     const file = (event.target as HTMLInputElement).files![0];
     this.fileName = file.name
     this.registerForm.get('resume')?.patchValue(file);
+  }
+
+  checkCaptcha(captchaResponse : any) {
+    this.captchaResolved = (captchaResponse && captchaResponse.length > 0) ? true : false
   }
 }
