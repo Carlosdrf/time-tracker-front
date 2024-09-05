b4e376abc31e23fe448265775882f7034e349a58
Sofia Bracho <sofia_bracho1@hotmail.com>
2024-09-05 11:21:20 -0400

    Removed info cards from client balance page, added a counter for the unpaid invoices, and removed transaction history from the transactions table

--- src/app/pages/client/client.balance/client.balance.component.ts
+++ src/app/pages/client/client.balance/client.balance.component.ts
@@ -5,7 +5,7 @@ import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import { SharedModule } from 'src/app/components/shared.module';
 import { PdfInvoiceService } from 'src/app/services/pdf-invoice.service';
-import { BalanceInfoComponent } from "../../../components/balance-info/balance-info.component";
+import { PendingInvoicesCounter } from 'src/app/components/pending-invoices-counter/pending-invoices-counter.component';
 import { BehaviorSubject } from 'rxjs';
 
 
@@ -14,13 +14,11 @@ import { BehaviorSubject } from 'rxjs';
   templateUrl: './client.balance.component.html',
   styleUrls: ['./client.balance.component.scss'],
   standalone: true,
-  imports: [SharedModule, MatTableModule, MatPaginatorModule, BalanceInfoComponent],
+  imports: [SharedModule, MatTableModule, MatPaginatorModule, PendingInvoicesCounter],
 })
 export class ClientBalanceComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-  payments: any[] = [];
   pending: any[] = [];
-  payments$: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
   pending$: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]);
 
   selectedPayment: any;
@@ -38,10 +36,6 @@ export class ClientBalanceComponent implements OnInit {
   ngOnInit() {
     this.getPayments();
     this.name = this.getUserName()
-    // Subscribe to the observables and update the arrays
-    this.payments$.subscribe(data => {
-      this.payments = this.payments.concat(data);
-    });
   }
   getPending() {
     if (this.pending && this.pending.length > 0) return true;
@@ -53,12 +47,8 @@ export class ClientBalanceComponent implements OnInit {
         this.pending = (v as Array<any>).filter(
           (payment: any) => payment.status.name === 'Pending'
         );
-        this.payments = (v as Array<any>).filter(
-          (payment: any) => payment.status.name === 'Active'
-        );
-        this.dataSource.data = this.pending.concat(this.payments);
+        this.dataSource.data = this.pending;
         this.dataSource.paginator = this.paginator;
-        this.payments$.next(this.payments);
         this.pending$.next(this.pending);
       },
     });
